3289289b82c0e0308708a3fcacea93bcbf748584
AbdulrahmanSalah <sa8640205@gmail.com>
2024-05-06 17:58:14 +0300

    .

--- library_app/obj/Debug/net8.0-windows/library_app.AssemblyInfo.cs
+++ library_app/obj/Debug/net8.0-windows/library_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f26be2179389de30a289ccac578e570d2ba61e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cbd50bed282827277b056262c2675b4049a2907")]
 [assembly: System.Reflection.AssemblyProductAttribute("library_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
